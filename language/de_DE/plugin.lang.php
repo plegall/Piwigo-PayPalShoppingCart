99c7c79de961f2bdcb1f8214e4b1cb8a1ff4c770
Sugar888 <Sugar888@piwigo.org>
2011-02-17 01:24:06 +0000

    [extension] Paypal shopping Cart - Added de_DE and it_IT
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@9256 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  Plugin Panier PayPal Pour Piwigo
+  Copyright (C) 2011 www.queguineur.fr — Tous droits réservés.
+  
+  Ce programme est un logiciel libre ; vous pouvez le redistribuer ou le
+  modifier suivant les termes de la “GNU General Public License” telle que
+  publiée par la Free Software Foundation : soit la version 3 de cette
+  licence, soit (à votre gré) toute version ultérieure.
+  
+  Ce programme est distribué dans l’espoir qu’il vous sera utile, mais SANS
+  AUCUNE GARANTIE : sans même la garantie implicite de COMMERCIALISABILITÉ
+  ni d’ADÉQUATION À UN OBJECTIF PARTICULIER. Consultez la Licence Générale
+  Publique GNU pour plus de détails.
+  
+  Vous devriez avoir reçu une copie de la Licence Générale Publique GNU avec
+  ce programme ; si ce n’est pas le cas, consultez :
+  <http://www.gnu.org/licenses/>.
+*/
+$lang['Action'] = 'Aktion';
+$lang['Add to cart'] = 'In den Einkaufswagen';
+$lang['Append photo size'] = 'Fotoformat anhängen';
+$lang['Append data'] = 'Daten anhängen';
+$lang['Buy this picture'] = 'Dieses Foto kaufen';
+$lang['Currency'] = 'Währung';
+$lang['Data appened'] = 'Daten angehängt';
+$lang['Data deleted'] = 'Daten gelöscht';
+$lang['Data updated'] = 'Daten aktualisiert';
+$lang['Delete data'] = 'Daten löschen';
+$lang['Fixed shipping cost'] = 'Fixe Versandkosten';
+$lang['PayPal Shopping Cart'] = 'PayPal Einkaufswagen';
+$lang['Price'] = 'Preis';
+$lang['Shipping cost'] = 'Versandkosten';
+$lang['Size'] = 'Grösse';
+$lang['Update data'] = 'Daten aktualisieren';
+$lang['View Shopping Cart'] = 'Einkaufswagen ansehen';
+$lang['View my PayPal Shopping Cart'] = 'Mein Paypal Einkaufswagen ansehen';
+?>
---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  Plugin Panier PayPal Pour Piwigo
+  Copyright (C) 2011 www.queguineur.fr — Tous droits réservés.
+  
+  Ce programme est un logiciel libre ; vous pouvez le redistribuer ou le
+  modifier suivant les termes de la “GNU General Public License” telle que
+  publiée par la Free Software Foundation : soit la version 3 de cette
+  licence, soit (à votre gré) toute version ultérieure.
+  
+  Ce programme est distribué dans l’espoir qu’il vous sera utile, mais SANS
+  AUCUNE GARANTIE : sans même la garantie implicite de COMMERCIALISABILITÉ
+  ni d’ADÉQUATION À UN OBJECTIF PARTICULIER. Consultez la Licence Générale
+  Publique GNU pour plus de détails.
+  
+  Vous devriez avoir reçu une copie de la Licence Générale Publique GNU avec
+  ce programme ; si ce n’est pas le cas, consultez :
+  <http://www.gnu.org/licenses/>.
+*/
+$lang['Action'] = 'Azione';
+$lang['Add to cart'] = 'Aggiungere al carrello';
+$lang['Append photo size'] = 'Aggiungere il formato della foto';
+$lang['Append data'] = 'Aggiungere i dati';
+$lang['Buy this picture'] = 'Comprare questa foto';
+$lang['Currency'] = 'Valuta';
+$lang['Data appened'] = 'Dati aggiunti';
+$lang['Data deleted'] = 'Dati cancellati';
+$lang['Data updated'] = 'Dati aggiornati';
+$lang['Delete data'] = 'Eliminare dati';
+$lang['Fixed shipping cost'] = 'Spese di spedizione fisse';
+$lang['PayPal Shopping Cart'] = 'Carrello PayPal';
+$lang['Price'] = 'Prezzo';
+$lang['Shipping cost'] = 'Spese di spedizione';
+$lang['Size'] = 'Formato';
+$lang['Update data'] = 'Aggiornare dati';
+$lang['View Shopping Cart'] = 'Visualizza carrello';
+$lang['View my PayPal Shopping Cart'] = 'Visualizzare il mio carrello Paypal';
+?>
