83e0b64f74215821efd2dea5662689f5bff1850f
dodo <dodo@piwigo.org>
2011-03-04 07:34:55 +0000

    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@9509 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  Plugin Panier PayPal Pour Piwigo
+  Copyright (C) 2011 www.queguineur.fr — Tous droits réservés.
+  
+  Ce programme est un logiciel libre ; vous pouvez le redistribuer ou le
+  modifier suivant les termes de la “GNU General Public License” telle que
+  publiée par la Free Software Foundation : soit la version 3 de cette
+  licence, soit (à votre gré) toute version ultérieure.
+  
+  Ce programme est distribué dans l’espoir qu’il vous sera utile, mais SANS
+  AUCUNE GARANTIE : sans même la garantie implicite de COMMERCIALISABILITÉ
+  ni d’ADÉQUATION À UN OBJECTIF PARTICULIER. Consultez la Licence Générale
+  Publique GNU pour plus de détails.
+  
+  Vous devriez avoir reçu une copie de la Licence Générale Publique GNU avec
+  ce programme ; si ce n’est pas le cas, consultez :
+  <http://www.gnu.org/licenses/>.
+*/
+$lang['Action'] = 'Akcia';
+$lang['Add to cart'] = 'Pridaj do košíka';
+$lang['Append photo size'] = 'Pridať veľkosť fotky';
+$lang['Append data'] = 'Pridať údaje';
+$lang['Buy this picture'] = 'Kúpiť fotku';
+$lang['Currency'] = 'Mena';
+$lang['Data appened'] = 'Údaje pridané';
+$lang['Data deleted'] = 'Údaje vymazané';
+$lang['Data updated'] = 'Údaje aktualizované';
+$lang['Delete data'] = 'Vymazané údaje';
+$lang['Fixed shipping cost'] = 'Pevná prepravná cena';
+$lang['PayPal Shopping Cart'] = 'PayPal nákupná košík';
+$lang['Price'] = 'Cena';
+$lang['Shipping cost'] = 'Cena prepravy';
+$lang['Size'] = 'Veľkosť';
+$lang['Update data'] = 'Aktualizovať údaje';
+$lang['View Shopping Cart'] = 'Ukázať nákupný košík';
+$lang['View my PayPal Shopping Cart'] = 'Ukázať môj nákupný košík';
+?>
