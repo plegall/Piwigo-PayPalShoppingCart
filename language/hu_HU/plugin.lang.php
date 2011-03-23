97d8c048df4605935c651034b865735dd071437b
samli <samli@piwigo.org>
2011-03-23 10:31:31 +0000

    [extensions] [PayPalShoppingCart] add HU language
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@9838 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  Plugin Panier PayPal Pour Piwigo
+  Copyright (C) 2011 www.queguineur.fr — Tous droits réservés.
+  
+  Ce programme est un logiciel libre ; vous pouvez le redistribuer ou le
+  modifier suivant les termes de la “GNU General Public License” telle que
+  publiée par la Free Software Foundation : soit la version 3 de cette
+  licence, soit (à votre gré) toute version ultérieure.
+  
+  Ce programme est distribué dans l’espoir qu’il vous sera utile, mais SANS
+  AUCUNE GARANTIE : sans même la garantie implicite de COMMERCIALISABILITÉ
+  ni d’ADÉQUATION À UN OBJECTIF PARTICULIER. Consultez la Licence Générale
+  Publique GNU pour plus de détails.
+  
+  Vous devriez avoir reçu une copie de la Licence Générale Publique GNU avec
+  ce programme ; si ce n’est pas le cas, consultez :
+  <http://www.gnu.org/licenses/>.
+*/
+$lang['Action'] = 'Művelet';
+$lang['Add to cart'] = 'Kosárba';
+$lang['Append photo size'] = 'Kép méretének hozzáfűzése';
+$lang['Append data'] = 'Adatok hozzáfűzése';
+$lang['Buy this picture'] = 'Kép vásárlása';
+$lang['Currency'] = 'Pénznem';
+$lang['Data appened'] = 'Adatok hozzáfűzve';
+$lang['Data deleted'] = 'Adat törölve';
+$lang['Data updated'] = 'Adat feltöltve';
+$lang['Delete data'] = 'Adat törlése';
+$lang['Fixed shipping cost'] = 'Fix szállítási költség';
+$lang['PayPal Shopping Cart'] = 'PayPal Shopping Cart';
+$lang['Price'] = 'Ár';
+$lang['Shipping cost'] = 'Szállítási költség';
+$lang['Size'] = 'Méret';
+$lang['Update data'] = 'Adatok frissítése';
+$lang['View Shopping Cart'] = 'Shopping Cart megtekintése';
+$lang['View my PayPal Shopping Cart'] = 'Nézze meg a PayPal Shopping Cart-ot';
+?>
