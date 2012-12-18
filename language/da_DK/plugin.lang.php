c8fbb087eccd0265fe6ea9e1f976422bb4580874
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-12-18 22:50:46 +0000

    [paypal_shopping_cart] Add da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@19493 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['all albums'] = 'alle albummer';
+$lang['Action'] = 'Handling';
+$lang['Add to cart'] = 'Føj til kurv';
+$lang['Append data'] = 'Tilføj data';
+$lang['Append photo size'] = 'Tilføj fotostørrelse';
+$lang['Apply to albums'] = 'Udfør på albummer';
+$lang['Buy this picture'] = 'Køb dette billede';
+$lang['Currency'] = 'Valuta';
+$lang['Data appened'] = 'Data tilføjet';
+$lang['Data deleted'] = 'Data slettet';
+$lang['Data updated'] = 'Data opdateret';
+$lang['Delete data'] = 'Slet data';
+$lang['Fixed shipping cost'] = 'Faste forsendelsesomkostninger';
+$lang['PayPal Shopping Cart'] = 'PayPal Shopping Cart';
+$lang['Price'] = 'Pris';
+$lang['Shipping cost'] = 'Forsendelsesomkostninger';
+$lang['Size'] = 'Størrelse';
+$lang['Update data'] = 'Opdater data';
+$lang['View Shopping Cart'] = 'Vis indkøbskurv';
+$lang['View my PayPal Shopping Cart'] = 'Vis min PayPal Shopping Cart';
+$lang['a list of albums'] = 'en liste over albummer';
+?>
