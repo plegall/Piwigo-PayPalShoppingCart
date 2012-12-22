21611674ac9bc8efb48977b162a1f65d3cf6547e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-12-22 18:13:41 +0000

    [paypal_shopping_cart] Add ru_RU, thanks to : nadusha
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@19526 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['all albums'] = 'все альбомы';
+$lang['Action'] = 'Действие';
+$lang['Add to cart'] = 'В Корзину';
+$lang['Append data'] = 'Дополнить данные';
+$lang['Append photo size'] = 'Добавить размер фото';
+$lang['Apply to albums'] = 'Пременить к альбому';
+$lang['Buy this picture'] = 'Купить фото';
+$lang['Currency'] = 'Валюта';
+$lang['Data appened'] = 'Данные дополнены';
+$lang['Data deleted'] = 'Данные удалены';
+$lang['Data updated'] = 'Данные изменены';
+$lang['Delete data'] = 'Удалить данные';
+$lang['Fixed shipping cost'] = 'Фиксированная стоимость доставки';
+$lang['PayPal Shopping Cart'] = 'Корзина PayPal';
+$lang['Price'] = 'Цена';
+$lang['Shipping cost'] = 'Стоимость доставки';
+$lang['Size'] = 'Размер';
+$lang['Update data'] = 'Изменить данные';
+$lang['View Shopping Cart'] = 'Посмотреть корзину';
+$lang['View my PayPal Shopping Cart'] = 'Посмотреть мою корзину PayPal';
+$lang['a list of albums'] = 'выбранные альбомы';
+?>
