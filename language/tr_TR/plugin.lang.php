0a97d9e7644166fb47bb60abb44bf8dae5c9cafd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-12-23 22:21:22 +0000

    [paypal_shopping_cart] Add tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@19540 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['all albums'] = 'tüm albümler';
+$lang['Action'] = 'Eylem';
+$lang['Add to cart'] = 'Sepete at';
+$lang['Append data'] = 'Bilgiyi birleştir';
+$lang['Append photo size'] = 'Eklenecek fotoğraf büyüklüğü';
+$lang['Apply to albums'] = 'Albümlere uygula';
+$lang['Buy this picture'] = 'Bu resmi satınal';
+$lang['Currency'] = 'Para birimi';
+$lang['Data appened'] = 'Veri eklendi';
+$lang['Data deleted'] = 'Veri silindi';
+$lang['Data updated'] = 'Veri güncellendi';
+$lang['Delete data'] = 'Veriyi sil';
+$lang['Fixed shipping cost'] = 'Sabit gönderi masrafı';
+$lang['PayPal Shopping Cart'] = 'PayPal Alışveriş Sepeti';
+$lang['Price'] = 'Fiyat';
+$lang['Shipping cost'] = 'Gönderi masrafı';
+$lang['Size'] = 'Büyüklük';
+$lang['Update data'] = 'Veriyi güncelle';
+$lang['View Shopping Cart'] = 'Alışveriş Sepetini Göster';
+$lang['View my PayPal Shopping Cart'] = 'PayPal Alışveriş Sepetimi Göster';
+$lang['a list of albums'] = 'albümlerin bir listesi';
+?>
