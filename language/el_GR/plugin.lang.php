73565e73cb15736e823adab7c17bd37621a90058
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-12-25 18:24:42 +0000

    [paypal_shopping_cart] Add el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@19568 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Data appened'] = 'Συνημμένα δεδομένα ';
+$lang['all albums'] = 'Όλα τα λευκώματα';
+$lang['a list of albums'] = 'μια λίστα των λευκωμέτων';
+$lang['View my PayPal Shopping Cart'] = 'Προβολή του PayPal Καλαθιού μου';
+$lang['View Shopping Cart'] = 'Προβολή Καλαθιού Αγορών';
+$lang['Update data'] = 'Ενημέρωση δεδομένων';
+$lang['Size'] = 'Μέγεθος';
+$lang['Shipping cost'] = 'Έξοδα αποστολής';
+$lang['Price'] = 'Τιμή';
+$lang['PayPal Shopping Cart'] = 'Καλάθι αγορών PayPal ';
+$lang['Fixed shipping cost'] = 'Πάγια έξοδα αποστολής';
+$lang['Delete data'] = 'Διαγραφή δεδομένων';
+$lang['Data updated'] = 'Ενημερωμένα δεδομένα';
+$lang['Data deleted'] = 'Διαγραμμένα δεδομένα ';
+$lang['Currency'] = 'Νόμισμα';
+$lang['Buy this picture'] = 'Αγοράστε αυτή την εικόνα';
+$lang['Apply to albums'] = 'Εφαρμόστε στα λευκώματα';
+$lang['Append photo size'] = 'Προσάρτηση μέγεθους φωτογραφίας';
+$lang['Append data'] = 'Προσάρτηση δεδομένων';
+$lang['Add to cart'] = 'Προσθήκη στο καλάθι';
+$lang['Action'] = 'Δράση';
+?>
