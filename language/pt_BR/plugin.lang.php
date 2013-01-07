b2f37a2eb252285b3a6bb9b25a1e6444a78b88de
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-07 17:48:34 +0000

    [paypal_shopping_cart] Add pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@19941 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['all albums'] = 'Todos os álbuns';
+$lang['Action'] = 'Ação';
+$lang['Add to cart'] = 'Adicione ao carrinho';
+$lang['Append data'] = 'Anexe dados';
+$lang['Append photo size'] = 'Anexar tamanho da foto';
+$lang['Apply to albums'] = 'Aplicar aos álbuns';
+$lang['Buy this picture'] = 'Compre essa foto';
+$lang['Currency'] = 'Moeda';
+$lang['Data appened'] = 'Dados anexados';
+$lang['Data deleted'] = 'Dados apagados';
+$lang['Data updated'] = 'Dados atualizados';
+$lang['Delete data'] = 'Apaga dados';
+$lang['Fixed shipping cost'] = 'Custo de transporte fixo';
+$lang['PayPal Shopping Cart'] = 'Carrinho de compras do PayPal';
+$lang['Price'] = 'Preço';
+$lang['Shipping cost'] = 'Custo de transporte';
+$lang['Size'] = 'Tamanho';
+$lang['Update data'] = 'Atualize dados';
+$lang['View Shopping Cart'] = 'Ver carrinho de compras';
+$lang['View my PayPal Shopping Cart'] = 'Ver meu carrinho de compras do PayPal';
+$lang['a list of albums'] = 'uma lista de álbuns';
+?>
