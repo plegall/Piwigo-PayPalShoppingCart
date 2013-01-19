dd250dfad5a6891e99b07746e993d6b2ae31b645
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-19 22:28:44 +0000

    [paypal_shopping_cart] Add zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@20273 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fixed shipping cost'] = '运费已修正';
+$lang['Action'] = '动作';
+$lang['all albums'] = '所有相册';
+$lang['Add to cart'] = '加入购物车';
+$lang['Append data'] = '添加数据';
+$lang['Append photo size'] = '添加图片尺寸';
+$lang['Apply to albums'] = '应用到相册';
+$lang['Buy this picture'] = '购买此图片';
+$lang['Currency'] = '货币';
+$lang['Data appened'] = '数据已添加';
+$lang['Data deleted'] = '数据已删除';
+$lang['Data updated'] = '数据已更新';
+$lang['Delete data'] = '删除数据';
+$lang['PayPal Shopping Cart'] = 'PayPal购物车';
+$lang['Price'] = '价格';
+$lang['Shipping cost'] = '运费';
+$lang['Size'] = '尺寸';
+$lang['Update data'] = '更新数据';
+$lang['View Shopping Cart'] = '查看购物车';
+$lang['View my PayPal Shopping Cart'] = '查看我的PayPal购物车';
+$lang['a list of albums'] = '相册列表';
+?>
