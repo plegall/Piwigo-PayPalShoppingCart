7adca58aaf4aee58da95ae52aaccc945b5345537
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-11 20:02:05 +0000

    [paypal_shopping_cart] Add pl_PL, thanks to : K.S.
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@20674 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Append data'] = 'Dodaj dane';
+$lang['Currency'] = 'Waluta';
+$lang['Data appened'] = 'Dane dodane';
+$lang['Data deleted'] = 'Dane usunięte';
+$lang['Data updated'] = 'Dane zaktualizowane';
+$lang['Delete data'] = 'Usuń dane';
+$lang['Shipping cost'] = 'Koszt dostawy';
+$lang['PayPal Shopping Cart'] = 'Koszyk z zakupami PayPal';
+$lang['Action'] = 'Akcja';
+$lang['Add to cart'] = 'Dodaj do koszyka';
+$lang['Append photo size'] = 'Dodaj rozmiar zdjęcia';
+$lang['Apply to albums'] = 'Zastosuj do albumów';
+$lang['a list of albums'] = 'lista albumów';
+$lang['all albums'] = 'wszystkie albumy';
+$lang['Buy this picture'] = 'Kup ten obraz';
+$lang['Price'] = 'Cena';
+$lang['Size'] = 'Rozmiar';
+$lang['Update data'] = 'Aktualizuj dane';
+$lang['View Shopping Cart'] = 'Pokaż koszyk z zakupami';
+$lang['View my PayPal Shopping Cart'] = 'Pokaż mój koszyk z zakupami PayPal';
+?>
