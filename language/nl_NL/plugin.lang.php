b66c823e0fe2694d05af440d3db257cf0035ad44
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-12 11:03:59 +0000

    [paypal_shopping_cart] Add nl_NL, thanks to : EdwinKort
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@20716 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['all albums'] = 'alle albums';
+$lang['Action'] = 'Actie';
+$lang['Add to cart'] = 'toevoegen aan winkelwagen';
+$lang['Append data'] = 'datum toegevoegd';
+$lang['Append photo size'] = 'Foto formaat';
+$lang['Apply to albums'] = 'Toepassen op albums';
+$lang['Buy this picture'] = 'Koop deze foto';
+$lang['Currency'] = 'Valuta';
+$lang['Data appened'] = 'gegevens toegevoegd';
+$lang['Data deleted'] = 'Datum verwijderd';
+$lang['Data updated'] = 'Datum bijgewerkt';
+$lang['Delete data'] = 'Verwijder gegevens';
+$lang['Fixed shipping cost'] = 'Vaste verzendkosten';
+$lang['PayPal Shopping Cart'] = 'Paypal winkelwagen';
+$lang['Price'] = 'Prijs';
+$lang['Shipping cost'] = 'Verzend kosten';
+$lang['Size'] = 'formaat';
+$lang['Update data'] = 'Data bijwerken';
+$lang['View Shopping Cart'] = 'Bekijk winkelwagen';
+$lang['View my PayPal Shopping Cart'] = 'Bekijk mijn PayPal winkelwagen';
+$lang['a list of albums'] = 'albumlijst';
+?>
