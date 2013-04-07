9a72b1dc3c8ff9f6689908fcd2cef88dd6c1f6c7
ddtddt <ddtddt@piwigo.org>
2013-04-07 06:28:03 +0000

    [extensions] - PayPalShoppingCart  -Add cs_CZ - Česky – Czech - Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@22049 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['Action'] = 'Akce';
+$lang['Add to cart'] = 'Přidat do košíku';
+$lang['Append photo size'] = 'Připojit velikost fotografie';
+$lang['Append data'] = 'Připojit data';
+$lang['Buy this picture'] = 'Koupit obrázek';
+$lang['Currency'] = 'Měna';
+$lang['Data appened'] = 'Data připojena';
+$lang['Data deleted'] = 'Data odstraněna';
+$lang['Data updated'] = 'Data aktualizována';
+$lang['Delete data'] = 'Odstranit data';
+$lang['Fixed shipping cost'] = 'Fixní cena dopravy';
+$lang['PayPal Shopping Cart'] = 'PayPal Shopping Cart';
+$lang['Price'] = 'Cena';
+$lang['Shipping cost'] = 'Cena dopravy';
+$lang['Size'] = 'Velikost';
+$lang['Update data'] = 'Aktualizovat data';
+$lang['View Shopping Cart'] = 'Zobrazit nákupní košík';
+$lang['View my PayPal Shopping Cart'] = 'Zobraz můj PayPal Shopping Cart';
+$lang['Apply to albums'] = 'Aplikovat na alba';
+$lang['all albums'] = 'všechny alba';
+$lang['a list of albums'] = 'seznam alb';
+?>
