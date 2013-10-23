8c57dbbbed9d735fbca0cc589c9c7b5b7ce3896a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-23 16:37:06 +0000

    [paypal_shopping_cart] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@25099 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['all albums'] = 'kõik albumid';
+$lang['Action'] = 'Tegevus';
+$lang['Add to cart'] = 'Lisa korvi';
+$lang['Append data'] = 'Lisa info';
+$lang['Append photo size'] = 'Lisa foto suurus';
+$lang['Apply to albums'] = 'Lisa albumisse';
+$lang['Buy this picture'] = 'Osta see pilt';
+$lang['Currency'] = 'Valuuta';
+$lang['Data appened'] = 'Info lisatud';
+$lang['Data deleted'] = 'Info kustutatud';
+$lang['Data updated'] = 'Info uuendatud';
+$lang['Delete data'] = 'Kustuta info';
+$lang['Fixed shipping cost'] = 'Fikseeritud postikulu';
+$lang['PayPal Shopping Cart'] = 'Paypal\'i ostukorv';
+$lang['Price'] = 'Hind';
+$lang['Shipping cost'] = 'Postikulu';
+$lang['Size'] = 'Suurus';
+$lang['Update data'] = 'Uuenda infot';
+$lang['View Shopping Cart'] = 'Vaata ostukorvi sisu';
+$lang['View my PayPal Shopping Cart'] = 'Vaata minu PayPali ostukorvi sisu';
+$lang['a list of albums'] = 'albumite nimekiri';
+?>
