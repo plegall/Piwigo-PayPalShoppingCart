5f23d85a6d4339da0a6fca90d37ac8b5128f1081
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-27 21:12:34 +0000

    [paypal_shopping_cart] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@25181 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Data appened'] = 'Дада додавання';
+$lang['Data deleted'] = 'Видалення даних';
+$lang['Data updated'] = 'Оновлення даних';
+$lang['Delete data'] = 'Видалення даних';
+$lang['Fixed shipping cost'] = 'Фіксована вартість доставки';
+$lang['PayPal Shopping Cart'] = 'PayPal кошик';
+$lang['Price'] = 'Ціна';
+$lang['Shipping cost'] = 'Вартість доставки';
+$lang['Size'] = 'Розмір';
+$lang['View my PayPal Shopping Cart'] = 'Переглянути мій PayPal кошик покупок';
+$lang['all albums'] = 'всі альбоми';
+$lang['Action'] = 'Дія';
+$lang['Add to cart'] = 'Додати в кошик';
+$lang['Append data'] = 'Додавання даних';
+$lang['Append photo size'] = 'Додати фото розміром';
+$lang['Apply to albums'] = 'Застосувати до альбомів';
+$lang['Buy this picture'] = 'Купити це фото';
+$lang['Currency'] = 'Валюта';
+$lang['Update data'] = 'Оновлення даних';
+$lang['View Shopping Cart'] = 'Переглянути кошик';
+$lang['a list of albums'] = 'список альбомів';
+?>
