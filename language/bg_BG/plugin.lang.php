6b8d4c5480e617d46f7494bd5aebc6a5c8ed4dfc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-27 21:12:40 +0000

    [paypal_shopping_cart] Add bg_BG, thanks to : neddrag
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@25185 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['View my PayPal Shopping Cart'] = 'Преглед на моята PayPal количка';
+$lang['all albums'] = 'всички албуми';
+$lang['Fixed shipping cost'] = 'Фиксирана цена на доставка';
+$lang['PayPal Shopping Cart'] = 'PayPal количка';
+$lang['Shipping cost'] = 'Цена на доставка';
+$lang['View Shopping Cart'] = 'Преглед на количката';
+$lang['a list of albums'] = 'списък с албуми';
+$lang['Price'] = 'Цена';
+$lang['Size'] = 'Размер';
+$lang['Update data'] = 'Обнови данните';
+$lang['Delete data'] = 'Изтрий данни';
+$lang['Data updated'] = 'Данните са обновени';
+$lang['Data deleted'] = 'Данните са изтрити';
+$lang['Data appened'] = 'Данните са добавени';
+$lang['Currency'] = 'Валута';
+$lang['Buy this picture'] = 'Купи тази снимка';
+$lang['Apply to albums'] = 'Приложи към албум';
+$lang['Append photo size'] = 'Добави размер';
+$lang['Append data'] = 'Добави данни';
+$lang['Add to cart'] = 'Добави към количката';
+$lang['Action'] = 'Действие';
+?>
