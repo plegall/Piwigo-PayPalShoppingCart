e15eda0e6934d99b61f80a0c1fb87ecc72e10467
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-16 18:18:51 +0000

    [paypal_shopping_cart] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@26810 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fixed shipping cost'] = '
+هزینه حمل و نقل ثابت است';
+$lang['PayPal Shopping Cart'] = '
+پی پال سبد خرید';
+$lang['Price'] = '
+قیمت';
+$lang['Shipping cost'] = 'هزینه حمل و نقل';
+$lang['Update data'] = 'بروز  رسانی اطلاعات';
+$lang['View Shopping Cart'] = 'دیدن سبد خرید';
+$lang['View my PayPal Shopping Cart'] = '
+دیدن  پی پال سبد من خرید';
+$lang['a list of albums'] = 'لیست  البوم ها ';
+$lang['Data appened'] = 'افزودن اطلاعات';
+$lang['Data deleted'] = 'اطلاعات حذف شد';
+$lang['Data updated'] = 'بروز رسانی اطلاعات';
+$lang['Delete data'] = 'حذف اطلاعات';
+$lang['Size'] = 'سایز ';
+$lang['Action'] = 'فعال';
+$lang['Add to cart'] = 'افزودن به سبد ';
+$lang['Append data'] = 'تاریخ پوستن';
+$lang['Append photo size'] = 'سایزعکس پیوست';
+$lang['Apply to albums'] = 'متصل کردن به البوم';
+$lang['Buy this picture'] = 'خریدن این عکس ';
+$lang['Currency'] = 'جاری';
+$lang['all albums'] = 'کل البوم ';
+?>
