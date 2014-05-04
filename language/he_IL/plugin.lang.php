4e0921d01c75b96c6f11b705e75480670a213dc3
ddtddt <ddtddt@piwigo.org>
2014-05-04 05:37:50 +0000

    [extensions] - PayPalShoppingCart - add he_IL - Hebrew - עברית - Thanks to ADIRBITON
    git-svn-id: http://piwigo.org/svn/extensions/PayPalShoppingCart@28338 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+/*
+    Plugin Panier PayPal Pour Piwigo
+    Copyright (C) 2011 www.queguineur.fr
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+$lang['Action']  = 'פעולה'; 
+$lang['Add to cart']  =   'הוסף לסל'; 
+$lang['Append photo size']  =   'צרף גודל תמונה '; 
+$lang['Append data']  =   'צירוף נתונים '; 
+$lang['Buy this picture']  =   'קנה תמונה זו'; 
+$lang['Currency']  =   'מטבע'; 
+$lang['Data appened']  =   'הנתונים צורפו'; 
+$lang['Data deleted']  =   'הנתונים נמחקו'; 
+$lang['Data updated']  =   'הנתונים עודכנו'; 
+$lang['Delete data']  =   'מחיקת נתונים'; 
+$lang['Fixed shipping cost']  =   'עלות משלוח קבועה'; 
+$lang['PayPal Shopping Cart']  =   'סל קניות PayPal '; 
+$lang['Price']  =   'המחיר'; 
+$lang['Shipping cost']  =   'עלות משלוח'; 
+$lang['Size']  =   'גודל'; 
+$lang['Update data']  =   'עדכון נתונים'; 
+$lang['View Shopping Cart']  =   'סל קניות'; 
+$lang['View my PayPal Shopping Cart']  =   'צפה בסל קניות PayPal שלי '; 
+$lang['Apply to albums']  =   'החל על אלבומים '; 
+$lang['all albums']  =   'כל האלבומים'; 
+$lang['a list of albums']  =   'רשימת האלבומים';
+?>
