fed59739e3cdbf98f32c2bb4cee8397e37b604bf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-03-07 06:21:06 +0100

    Update ca_ES, thanks to Sebastia

--- language/ca_ES/plugin.lang.php
+++ language/ca_ES/plugin.lang.php
@@ -6,3 +6,23 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['all albums'] = 'tots els àlbums';
+$lang['Shipping cost'] = 'Cost d\'enviament';
+$lang['Size'] = 'Mida';
+$lang['Update data'] = 'Actualitza les dades';
+$lang['View Shopping Cart'] = 'Veure la cistella de la compra';
+$lang['View my PayPal Shopping Cart'] = 'Veure la meva targeta de compra PayPal';
+$lang['a list of albums'] = 'una llista d\'àlbums';
+$lang['Action'] = 'Acció';
+$lang['Add to cart'] = 'Afegeix a la cistella';
+$lang['Append data'] = 'Afegeix dades';
+$lang['Append photo size'] = 'Afegeix una mida de foto';
+$lang['Apply to albums'] = 'Aplica als àlbums';
+$lang['Buy this picture'] = 'Compra aquesta imatge';
+$lang['Currency'] = 'Moneda';
+$lang['Data appened'] = 'Dades apreses';
+$lang['Data deleted'] = 'Dades suprimides';
+$lang['Data updated'] = 'Dades actualitzades';
+$lang['Delete data'] = 'Suprimeix les dades';
+$lang['Fixed shipping cost'] = 'Cost fix d\'enviament';
+$lang['PayPal Shopping Cart'] = 'PayPal Shopping Cart';
+$lang['Price'] = 'Preu';
