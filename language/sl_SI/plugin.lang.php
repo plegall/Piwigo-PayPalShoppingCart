a47b095a4273c8b531d3b464e0769d482c3b404f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-17 07:56:28 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Fixed shipping cost'] = 'Fiksni stroški pošiljanja';
+$lang['PayPal Shopping Cart'] = 'PayPal nakupovalni voziček';
+$lang['Price'] = 'Cena';
+$lang['Shipping cost'] = 'Stroški pošiljanja';
+$lang['Size'] = 'Velikost';
+$lang['Update data'] = 'Posodobite podatke';
+$lang['View Shopping Cart'] = 'Poglej nakupovalni voziček';
+$lang['View my PayPal Shopping Cart'] = 'Poglej moj PayPal nakupovalni voziček';
+$lang['a list of albums'] = 'seznam albumov';
+$lang['all albums'] = 'vsi albumi';
+$lang['Action'] = 'Akcija';
+$lang['Add to cart'] = 'Dodaj v voziček';
+$lang['Append data'] = 'Dodaj podatke';
+$lang['Append photo size'] = 'Dodaj velikost fotografije';
+$lang['Apply to albums'] = 'Uporabi za albume';
+$lang['Buy this picture'] = 'Kupi to sliko';
+$lang['Currency'] = 'Valuta';
+$lang['Data appened'] = 'Dodajanje podatkov';
+$lang['Data deleted'] = 'Izbriši podatke';
+$lang['Data updated'] = 'odatki posodobljeni';
+$lang['Delete data'] = 'Izbriši podatke';
