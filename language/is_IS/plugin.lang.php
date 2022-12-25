2e12b09b79842dec2d11143c4ac93ecca32d0675
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-25 07:48:20 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply to albums'] = 'Sækja um albúm';
+$lang['Buy this picture'] = 'Kaupa þessa mynd';
+$lang['Currency'] = 'Gjaldmiðill';
+$lang['Data appened'] = 'Gögn viðbætt';
+$lang['Data deleted'] = 'Gögn eydd';
+$lang['Data updated'] = 'Gögn uppfærð';
+$lang['Delete data'] = 'Eyða gögnum';
+$lang['Fixed shipping cost'] = 'Fastur sendingarkostnaður';
+$lang['PayPal Shopping Cart'] = 'PayPal innkaupakarfa';
+$lang['Price'] = 'Verð';
+$lang['Shipping cost'] = 'Sendingarkostnaður';
+$lang['Size'] = 'Stærð';
+$lang['Update data'] = 'Uppfæra gögn';
+$lang['View Shopping Cart'] = 'Skoða innkaupakörfu';
+$lang['View my PayPal Shopping Cart'] = 'Skoða mína PayPal innkaupakörfu';
+$lang['a list of albums'] = 'listi yfir albúm';
+$lang['all albums'] = 'öll albúm';
+$lang['Action'] = 'Aðgerð';
+$lang['Add to cart'] = 'Bæta í körfu';
+$lang['Append data'] = 'Bæta við gögnum';
+$lang['Append photo size'] = 'Bæta við myndastærð';
