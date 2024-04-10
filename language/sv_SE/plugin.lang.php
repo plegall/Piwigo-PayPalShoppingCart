15000869a01f42e9ae96ac9978d46eb32f16ab78
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-10 08:29:08 +0200

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Action'] = 'Åtgärd';
+$lang['Append data'] = 'Lägg till data';
+$lang['Append photo size'] = 'Lägg till fotostorlek';
+$lang['Data appened'] = 'Data tillagd';
+$lang['Data deleted'] = 'Data raderad';
+$lang['Data updated'] = 'Data uppdaterad';
+$lang['Delete data'] = 'Radera data';
+$lang['Update data'] = 'Uppdatera data';
+$lang['all albums'] = 'alla album';
+$lang['Add to cart'] = 'Lägg i kundvagn';
+$lang['Apply to albums'] = 'Applicera på album';
+$lang['Buy this picture'] = 'Köp denna bild';
+$lang['Currency'] = 'Valuta';
+$lang['Fixed shipping cost'] = 'Fast fraktkostnad';
+$lang['PayPal Shopping Cart'] = 'Paypal kundvagn';
+$lang['Price'] = 'Pris';
+$lang['Shipping cost'] = 'Fraktkostnad';
+$lang['Size'] = 'Storlek';
+$lang['View Shopping Cart'] = 'Visa kundvagn';
+$lang['View my PayPal Shopping Cart'] = 'Visa min Paypal kundvagn';
+$lang['a list of albums'] = 'en lista på album';
