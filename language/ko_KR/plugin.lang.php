1a4c1d4a6b7df8af07fa749161ec97bb2557b123
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-06 17:44:43 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['all albums'] = '모든 앨범';
+$lang['Action'] = '작업';
+$lang['Add to cart'] = '장바구니에 담기';
+$lang['Append data'] = '데이터 추가';
+$lang['Append photo size'] = '사진 크기 추가';
+$lang['Apply to albums'] = '앨범에 적용';
+$lang['Buy this picture'] = '이 사진 구매하기';
+$lang['Currency'] = '통화';
+$lang['Data appened'] = '데이터 추가됨';
+$lang['Data deleted'] = '데이터 삭제됨';
+$lang['Data updated'] = '데이터 업데이트됨';
+$lang['Delete data'] = '데이터 삭제';
+$lang['Fixed shipping cost'] = '고정 배송비';
+$lang['PayPal Shopping Cart'] = 'PayPal 장바구니';
+$lang['Price'] = '가격';
+$lang['Shipping cost'] = '배송비';
+$lang['Size'] = '크기';
+$lang['Update data'] = '데이터 업데이트';
+$lang['View Shopping Cart'] = '장바구니 보기';
+$lang['View my PayPal Shopping Cart'] = '내 PayPal 장바구니 보기';
+$lang['a list of albums'] = '앨범 목록';
